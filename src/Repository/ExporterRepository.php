fc680eb032e7504acf13da8d1023d9ba453cd1f4
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2019-10-23 15:58:57 +0200

    Fix gauges

--- src/Repository/ExporterRepository.php
+++ src/Repository/ExporterRepository.php
@@ -5,6 +5,8 @@ namespace LKDevelopment\HorizonPrometheusExporter\Repository;
 
 
 use LKDevelopment\HorizonPrometheusExporter\Contracts\Exporter;
+use Prometheus\CollectorRegistry;
+use Prometheus\Storage\InMemory;
 
 class ExporterRepository
 {
@@ -12,14 +14,14 @@ class ExporterRepository
     public static function load(){
         $exporters = config('horizon-exporter.exporters');
 
-        self::$registry = \Prometheus\CollectorRegistry::getDefault();
+        self::$registry = new CollectorRegistry(new InMemory());
         foreach ($exporters as $exporter){
             $_exporter = new $exporter();
             /**
              * @var Exporter $_exporter
              */
             $_exporter->metrics(self::$registry);
-            $_exporter->collect(self::$registry);
+            $_exporter->collect();
         }
     }
     public static function getRegistry(){
